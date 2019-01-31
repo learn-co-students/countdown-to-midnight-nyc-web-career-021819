951d20050dffe80c30fd815ab879d58c3601718f
sensible-shell-8529 <diana.lee116@gmail.com>
2019-01-31 18:42:47 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,12 @@
-#write your code here
+def countdown (number)
+  while number >= 0 
+  puts "#{number} SECOND(S)!"
+  number -=1
+  break if number == 0 
+end
+  return "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep(number)
+  sleep(number)
 end
