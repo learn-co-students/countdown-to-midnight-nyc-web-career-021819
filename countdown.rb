5dda7c14f89d1318d9397bb2ee58acb3e20342ee
authentic-actor-8801 <xaviernational@aol.com>
2019-02-11 05:03:02 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(x)
+  number = x
+  for counter in 1..x do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(x)
+  sleep x
+  number = x
+  for counter in 0..x do
+    puts "#{number} SECOND(S)!"
+    number -= 1
+  end
+  "HAPPY NEW YEAR!"
 end
